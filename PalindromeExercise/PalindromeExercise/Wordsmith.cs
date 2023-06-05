bda9b0e0ec62096f7b6ed54e72e80821e1ca12c3
Joseph LaVia <josephdlavia@gmail.com>
2023-06-05 14:32:22 -0500

    PalindromeComplete

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class Wordsmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            string wordLowered = word.ToLower();
+            string backwards = "";
+
+            for (int i = wordLowered.Length - 1; i >= 0; i--)
+            {
+                backwards += wordLowered[i];
+            }
+            if (wordLowered == backwards)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
